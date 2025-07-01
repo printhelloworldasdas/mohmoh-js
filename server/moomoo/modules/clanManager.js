8c04830fc87c4477c10d494086d43bb5ffd00af3
printhelloworldasdas <165765404+printhelloworldasdas@users.noreply.github.com>
2025-07-01 12:59:09 +0200

    Update clanManager.js

--- server/moomoo/modules/clanManager.js
+++ server/moomoo/modules/clanManager.js
@@ -1,188 +1,26 @@
-
-import { Player } from "./player.js";
-
-export class Clan {
-
-    teams = new Map;
-    /** @type {Player[]} */
-    players = 0xf;
-    /** @type {Player} */
-    owner = -1;
-
-    constructor(title, players, owner) {
-
-        this.title = title;
-        this.players = players;
-        this.owner = owner;
-
-        this.join(owner.sid, owner.name);
-
-    }
-
-    join(sid, name) {
-
-        const player = this.players.find(x => x.sid === sid);
-
-        if (!player) return;
-
-        if (player.team) return;
-
-        player.team = this.title;
-        player.send("st", player.team, player.is_owner);
-
-        this.teams.set(
-            sid,
-            name
-        );
-
-        this.update();
-
-    }
-
-    kick(sid) {
-
-        const player = this.players.find(x => x.sid === sid);
-
-        if (!player) return;
-
-        player.team = null;
-        player.send("st", null, false);
-
-        if (this.teams.delete(sid)) this.update();
-
-    }
-
-    notify(sid) {
-
-        if (this.owner.notify.has(sid)) return;
-
-        const player = this.players.find(x => x.sid === sid);
-        if (!player) return;
-
-        this.owner.send("an", sid, player.name);
-        this.owner.notify.add(sid);
-
-    }
-
-    update() {
-
-        const ext = [...this.teams.entries()].flat();
-
-        for (const [sid, _name] of this.teams) {
-
-            const player = this.players.find(x => x.sid === sid);
-
-            if (!player) break;
-
-            player.send("sa", ext);
-
-        }
-
-    }
-
-}
-
-export class ClanManager {
-
-    clans = new Map;
-
-    /** @type {Player[]} */
-    players = 0xff;
-
-    constructor(players, server) {
-
-        this.players = players;
-        this.server = server;
-
-    }
-
-    create(name, player) {
-
-        if (this.clans.has(name)) return false;
-
-        const clan = new Clan(name, this.players, player);
-
-        this.clans.set(name, clan);
-        this.server.broadcast("ac", {sid: name});
-
-        return true;
-
-    }
-
-    remove(name) {
-
-        const clan = this.clans.get(name);
-
-        if (!clan) return false;
-
-        for (const [sid, name] of clan.teams) {
-            
-            const player = this.players.find(x => x.sid === sid);
-
-            if (!player) break;
-
-            player.team = null;
-            player.is_owner = false;
-            player.send("st", null, false);
-
-        }
-
-        clan.owner.notify.clear();
-
-        this.clans.delete(name);
-        this.server.broadcast("ad", name);
-
-        return true;
-
-    }
-
-    kick(name, sid) {
-
-        this.clans.get(name)?.kick(sid);
-
-    }
-
-    join(name, player) {
-
-        this.clans.get(name)?.join(player.sid, player.name);
-
-    }
-
-    add_notify(name, sid) {
-
-        this.clans.get(name)?.notify(sid);
-
-    }
-
-    unnotify(name, sid) {
-
-        this.clans.get(name)?.owner.notify.delete(sid);
-
-    }
-
-    confirm_join(name, sid, join) {
-
-        const player = this.players.find(x => x.sid === sid);
-
-        if (!player) return;
-
-        this.unnotify(name, player?.sid);
-
-        if (join) {
-            this.join(name, player);
-            return;
-        }
-
-    }
-
-    ext() {
-
-        const ext = [...this.clans.keys()].map(sid => {
-            return {sid}
-        });
-
-        return ext;
-
-    }
-
+module.exports = function (Tribe, findPlayerBySID, server) {
+	this.tribes = {}
+	this.createTribe = (name, player) => {
+		const newTribe = new Tribe(name, findPlayerBySID, server)
+		this.tribes[name] = newTribe
+		newTribe.addPlayer(player)
+		return newTribe
+	}
+
+	this.deleteTribe = (name) => {
+		const tmpTribe = this.tribes[name]
+		if (tmpTribe) {
+			for (let i = 0; i < tmpTribe.members.length; i++) {
+				const tmpPlayer = findPlayerBySID(tmpTribe.members[i])
+				tmpPlayer.team = null
+				tmpPlayer.isLeader = false
+				server.send(tmpPlayer.id, "st", [null, 0])
+			}
+			delete this.tribes[name]
+		}
+	}
+
+	this.getTribe = (name) => {
+		return this.tribes[name]
+	}
 }
